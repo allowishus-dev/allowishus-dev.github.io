c440c04c9e5277c0cca0a89753d946ca43905f1d
allowishus.dev <allowishus.dev@gmail.com>
2019-10-24 09:35:30 +0200

    Updates

---
+++ precache-manifest.0535b7cfd397f6703095689836db36e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02752145dcc5bfe1242c41f31c255530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ea00765d88b370d47e3",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "a820a8f584fa07238bab",
+    "url": "/static/css/main.8436032c.chunk.css"
+  },
+  {
+    "revision": "2ea00765d88b370d47e3",
+    "url": "/static/js/2.c6a5568e.chunk.js"
+  },
+  {
+    "revision": "a820a8f584fa07238bab",
+    "url": "/static/js/main.6522b82c.chunk.js"
+  },
+  {
+    "revision": "d9091079ad72bb325bd9",
+    "url": "/static/js/runtime-main.493472ac.js"
+  }
+]);
